03b66cd126f7fff46a4a664710b1b318179facae
Victor Chee <victor.qhj@gmail.com>
2016-05-19 09:06:39 +0800

    Add project

---
+++ AVPlayerDemo/AVPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayerDemo
+//
+//  Created by Victor Chee on 16/5/19.
+//  Copyright © 2016年 VictorChee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
